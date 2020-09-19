06dd30e6bc5eebcb044308dcfddf8272130fc919
liyuxing123123 <lyx_liyuxing@163.com>
2020-09-19 21:02:37 +0800

    test1212321233123

--- test006.py
+++ test006.py
@@ -17,4 +17,19 @@ for roll_num in range(1000):
 # 分析结果
 frequencies = []
 max_result = de_1.num_sides + de_2.num_sides
+for value in range(2, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# 可视化结果
+hist = pygal.Bar()
+
+hist.title = "Results of rolling two D6 dice 1000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 + D6', frequencies)
+hist.render_to_file('de_visual.svg')
+
 
